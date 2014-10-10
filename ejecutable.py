c9f28db2559d40bd0c3db5a41142b170a382ea50
ALBERTO COLMAN <albertocolman81@gmail.com>
2014-10-10 14:35:29 -0300

    otros

--- ejecutable.py
+++ ejecutable.py
@@ -6,10 +6,14 @@ build_exe_options = {"excludes": ["tkinter"],"include_files":["img","views","pic
 base = None
 if sys.platform == "win32":
     base = "Win32GUI"
+    executable = [Executable("MCGestionClientes.py", base=base, icon="img\\zumbaIcon.ico")]
+else:
+    executable = [Executable("MCGestionClientes.py", base=base, icon="img/zumbaIcon.ico")]
+
 
 setup(name = "MC Gestion Clientes",
 	  version = "1.0",
 	  author="Alberto Denis Colman Meza",
 	  description = "Sistema de gestion de clientes para locales de dance and fitness",
 	  options = {"build_exe": build_exe_options},
-	  executables = [Executable("MCGestionClientes.py", base=base, icon="img\\zumbaIcon.ico")],)
+	  executables = executable,)
